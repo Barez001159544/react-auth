8b76a505fd8c8a398c756d8adb9bbcde6070c4b7
barez azad <barez.001159544@gmail.com>
2025-04-01 21:02:23 +0300

    First Commit

---
+++ src/Components/LoginSignUp/LoginSignUp.jsx
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+import "./LoginSignUp.css";
+import email_icon from "../Assets/mail.png";
+import user_icon from "../Assets/user.png";
+import password_icon from "../Assets/padlock.png";
+import axios from "axios";
+import { ThreeDot } from "react-loading-indicators";
+
+const LoginSignUp = () => {
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = async () => {
+    console.log(email); //emilys
+    console.log(password); //emilyspass
+    setLoading(true);
+    setErrorMessage("");
+    try {
+      const response = await axios.post(
+        "https://dummyjson.com/auth/login",
+        {
+          username: email,
+          password: password,
+        },
+        {
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+
+      console.log(response.data);
+      setLoading(false);
+    } catch (error) {
+      console.log("Failed to login");
+      setLoading(false);
+      if (error.response?.status === 400) {
+        setErrorMessage(
+          "The username or password is wrong, try to change either of them, check then try again."
+        );
+      } else {
+        setErrorMessage(
+          "Something went wrong that has nothing to do with the email or password. Please do try agian later."
+        );
+      }
+      setTimeout(() => {
+        setErrorMessage("");
+      }, 3000);
+    }
+  };
+
+  return (
+    <div className="container">
+      <div className="header">
+        <div className="text">Login</div>
+        <div className="underline"></div>
+      </div>
+      <div className="inputs">
+        <div className="input">
+          <img src={email_icon} alt="" />
+          <input
+            type="email"
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="input">
+          <img src={password_icon} alt="" />
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+      </div>
+      <div className="forgot-password">
+        Lost Password? <span>Click Here</span>
+      </div>
+      <div className="submit-container">
+        <div className="submit" onClick={() => login()}>
+          Login
+        </div>
+      </div>
+      {/* Loading Popup */}
+      {loading && (
+        <div className="popup loading-popup">
+          <ThreeDot color="#fff" size="large" />
+        </div>
+      )}
+      {errorMessage && (
+        <div className="popup error-popup">
+          <h3>AN ERROR OCURRED</h3>
+          <p>{errorMessage}</p>
+          <div className="submit confirm" onClick={() => setErrorMessage("")}>
+            OK
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default LoginSignUp;
